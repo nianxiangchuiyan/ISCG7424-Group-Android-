33776b49ea046d0893ab3436165e2a4a04164185
Freeman-HP <kingkongenzo@gmail.com>
2025-10-27 12:39:41 +1300

    init design of Booking model

---
+++ app/src/main/java/com/dinecraft/app/Booking.java
@@ -0,0 +1,89 @@
+package com.dinecraft.app;
+
+import java.util.Date;
+
+public class Booking {
+    private String booking_id;
+    private Date date;
+    private int timeslot;
+    private int seat_required;
+    private Table table;
+    private String table_name;
+    private String pref_name;
+    private String contact_number;
+    private String memo;
+
+    public Booking(){}
+
+    public String getBooking_id() {
+        return booking_id;
+    }
+
+    public void setBooking_id(String booking_id) {
+        this.booking_id = booking_id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public int getTimeslot() {
+        return timeslot;
+    }
+
+    public void setTimeslot(int timeslot) {
+        this.timeslot = timeslot;
+    }
+
+    public int getSeat_required() {
+        return seat_required;
+    }
+
+    public void setSeat_required(int seat_required) {
+        this.seat_required = seat_required;
+    }
+
+    public Table getTable() {
+        return table;
+    }
+
+    public void setTable(Table table) {
+        this.table = table;
+    }
+
+    public String getTable_name() {
+        return table_name;
+    }
+
+    public void setTable_name(String table_name) {
+        this.table_name = table_name;
+    }
+
+    public String getPref_name() {
+        return pref_name;
+    }
+
+    public void setPref_name(String pref_name) {
+        this.pref_name = pref_name;
+    }
+
+    public String getContact_number() {
+        return contact_number;
+    }
+
+    public void setContact_number(String contact_number) {
+        this.contact_number = contact_number;
+    }
+
+    public String getMemo() {
+        return memo;
+    }
+
+    public void setMemo(String memo) {
+        this.memo = memo;
+    }
+}
