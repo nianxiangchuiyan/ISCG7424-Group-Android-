e5430f9ef25854648b64afe5a01124d7f52dcc06
yeeeeeeeeeeeeeeeeeeee <65873278+yeeeeeeeeeeeeeeeeeeee@users.noreply.github.com>
2025-10-29 21:46:39 +1300

    Categories.we need the drawables.

---
+++ app/src/main/java/com/dinecraft/app/CategoriesActivity.java
@@ -0,0 +1,35 @@
+package com.dinecraft.app;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CategoriesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_categories);
+
+        RecyclerView recyclerView = findViewById(R.id.recycler_categories);
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
+
+        List<Category> categoryList = new ArrayList<>();
+        categoryList.add(new Category("Pizza", R.drawable.pizza));
+        categoryList.add(new Category("Burgers", R.drawable.burger));
+        categoryList.add(new Category("Drinks", R.drawable.drinks));
+        categoryList.add(new Category("Desserts", R.drawable.dessert));
+
+        CategoryAdapter adapter = new CategoryAdapter(categoryList, category ->
+                Toast.makeText(this, "Clicked " + category.getName(), Toast.LENGTH_SHORT).show()
+        );
+
+        recyclerView.setAdapter(adapter);
+    }
+}
---
+++ app/src/main/java/com/dinecraft/app/CategoryAdapter.java
@@ -0,0 +1,59 @@
+package com.dinecraft.app;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.ViewHolder> {
+
+    private List<Category> categoryList;
+    private OnCategoryClickListener listener;
+
+    public interface OnCategoryClickListener {
+        void onCategoryClick(Category category);
+    }
+
+    public CategoryAdapter(List<Category> categoryList, OnCategoryClickListener listener) {
+        this.categoryList = categoryList;
+        this.listener = listener;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_category, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Category category = categoryList.get(position);
+        holder.name.setText(category.getName());
+        holder.image.setImageResource(category.getImageResId());
+        holder.itemView.setOnClickListener(v -> listener.onCategoryClick(category));
+    }
+
+    @Override
+    public int getItemCount() {
+        return categoryList.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView image;
+        TextView name;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            image = itemView.findViewById(R.id.category_image);
+            name = itemView.findViewById(R.id.category_name);
+        }
+    }
+}
